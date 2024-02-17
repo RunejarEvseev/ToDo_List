2ee9abf50b282b01d2a67a4b28a10625b77847cf
Artyom Evseev <a.euseew2011@yandex.ru>
2024-02-17 22:10:37 +0300

    add task.id !== id

--- js/main.js
+++ js/main.js
@@ -24,17 +24,18 @@ function addTask(event) {
     id: Date.now(),
     text: taskText,
     done: false,
-  }
+  };
 
   //Добавляем задачу в массив с задачами
   tasks.push(newTask)
 
-  
+  // Формируем CSS класс
+  const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
 
   // Формируем разметку для новой задачи
   const taskHTML = `
-    <li class="list-group-item d-flex justify-content-between task-item">
-      <span class="task-title">${taskText}</span>
+    <li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+      <span class="${cssClass}">${newTask.text}</span>
       <div class="task-item__buttons">
         <button type="button" data-action="done" class="btn-action">
           <img src="./img/tick.svg" alt="Done" width="18" height="18" />
@@ -62,8 +63,22 @@ function deleteTask(event) {
   // Проверяем если клик был НЕ по кнопке "удалить задачу"
   if (event.target.dataset.action !== 'delete') return;
   
-  // Проверяем, что клик был по кнопке "удалить задачу"
+ 
   const parentNode = event.target.closest('.list-group-item');
+
+  // Определяем ID задачи
+  const id = Number(parentNode.id);
+  
+  // Находим индекс задачи в массиве
+  // const index = tasks.findIndex((task) => task.id === id);
+
+  // Удаляем задачу из массива с задачами
+  // tasks.splice(index, 1)
+
+  // Удаляем задачу через фильтрацию массива
+  tasks = tasks.filter((task) => task.id !== id)
+
+  // Удаляем задачу из разметки
   parentNode.remove();
 
   // Проверка. Если в списке задач 1-ин элемент, то блок "Список дел пуст" отображается
